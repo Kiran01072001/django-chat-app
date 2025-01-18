f704006678612a1f6abd5b2cb657adafb4b68976
Maradana Kiran <131331920+Kiran01072001@users.noreply.github.com>
2025-01-18 19:49:38 +0530

    Update settings.py

--- Chat Application/chat_project/settings.py
+++ Chat Application/chat_project/settings.py
@@ -1,6 +1,12 @@
 # Add this to the existing settings.py content
+
 INSTALLED_APPS = [
-    ...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',  # This is required for static files
     'chat',
     'channels',  # Add this for WebSocket support
 ]
@@ -12,6 +18,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # This is where collectstatic will collect static files
 
 # ASGI configuration for WebSocket
 ASGI_APPLICATION = 'chat_project.asgi.application'
